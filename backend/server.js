7da143837edaf901c68d962c04af763a9094962f
hi2suresh <hi2suresh@gmail.com>
2025-07-10 06:45:54 +1000

    completed the app

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,21 @@ app.use(express.json());
 
 app.use('/api/products/', productRouter);
 
+const __dirname = process.cwd();
+// Serve static files from the React app
+app.use(express.static(__dirname + '/frontend/dist'));
+
+// Handle React routing, return all requests to React app
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/frontend/dist/index.html', (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+}
+
 app.listen(port, () => {
   connectDB();
   console.log('Express server started on port 5000...');
